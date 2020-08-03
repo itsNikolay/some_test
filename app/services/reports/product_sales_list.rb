9c49611fce093f7efba22dbc15541e1dac0c16d0
Nikolay Ponomarev <itsnikolay@gmail.com>
2020-08-03 15:50:17 +0300

    impr

--- app/services/reports/product_sales_list.rb
+++ app/services/reports/product_sales_list.rb
@@ -9,22 +9,24 @@ module Reports
     end
 
     def call
-      @products = get_products.map do |product|
+      @products = build_products.map do |product|
         [product, amounts_by_product_id[product.id]]
       end
     end
 
     private
 
-    def get_products
-      Product.distinct.all
+    def build_products
+      Product
+        .distinct
+        .all
         .joins(order_items: :order)
         .where(order_items: { orders: { id: order_ids } })
         .order(:id)
     end
 
     def amounts_by_product_id
-      @amounts_by_product_id ||= 
+      @amounts_by_product_id ||=
         product_orders.each_with_object({}) do |pr, obj|
           product_id = pr[0]
           order_id = pr[1]
@@ -34,7 +36,7 @@ module Reports
     end
 
     def product_orders
-      get_products.pluck(:id, 'orders.id')
+      build_products.pluck(:id, 'orders.id')
     end
 
     def order_ids
@@ -42,16 +44,7 @@ module Reports
     end
 
     def amounts_by_order_id
-      @amounts_by_order_id ||=
-        amounts.each_with_object({}) do |am, obj|
-          order_id = am[0]
-          amount = am[1]
-          obj[order_id] = (obj[order_id] || 0) + amount
-        end
-    end
-
-    def amounts
-      payments.pluck(:order_id, :amount)
+      @amounts_by_order_id ||= payments.group(:order_id).sum(:amount)
     end
 
     def payments
